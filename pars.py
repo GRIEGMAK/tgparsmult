d70a27a20a3a1d5daacd199c7b87c517334204eb
GRIEGMAK <g.m.ak@yandex.ru>
2021-11-23 11:45:52 +0600

    исправил парс

--- pars.py
+++ pars.py
@@ -33,6 +33,8 @@ def connectionTelegramAccount(phone, api_id, api_hash):
         i += 1
     if(csv_accounts == []):
         proxy_number = int(input("Введите номер прокси которой будет использоваться, Если вы не хотите использовать прокси введите 0 "))
+    else:
+        proxy_number = 0
     if proxy_number != 0:
         for cpass in csv_accounts:
             if numbering == proxy_number:
