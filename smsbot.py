0b7ffeddae43661bd30eb86118896ee3a2e978b2
GRIEGMAK <g.m.ak@yandex.ru>
2021-12-03 08:44:10 +0600

    начал редачить smsbot

--- smsbot.py
+++ smsbot.py
@@ -8,6 +8,7 @@ import csv
 import random
 import time
 from telethon import connection
+import python_socks
 
 re="\033[1;31m"
 gr="\033[1;32m"
@@ -22,55 +23,68 @@ class main():
     {re}╔╦╗{cy}┌─┐┌─┐┌─┐┌─┐┬─┐{re}╔═╗
     {re} ║ {cy}├─┐├┤ ├─┘├─┤├┬┘{re}╚═╗
     {re} ╩ {cy}└─┘└─┘┴  ┴ ┴┴└─{re}╚═╝
-    by https://github.com/elizhabs
             """)
 
     def connectionTelegramAccount(phone, api_id, api_hash):
-        csv_accounts = csv.reader(open('proxy.csv', "r"), delimiter=",")
-        g = 0
-        numbering = 1
-        i = 1
         file_name = 'proxy.csv'
         if not os.path.exists(file_name):
             f = open(file_name, "w")
             f.close()
+        csv_accounts = csv.reader(open('proxy.csv', "r"), delimiter=",")
+        g = 0
+        numbering = 1
+        i = 1
         for cpass in csv_accounts:
             print (i, ")", cpass[0], cpass[1])
             i += 1
-        proxy_number = int(input("Введите номер прокси которой будет использоваться, Если вы не хотите использовать прокси введите 0 "))
+        proxy_number = 0
+        sys.stdout.write('\r\a')
+        sys.stdout.flush()
+        if(i > 1):
+            proxy_number = input("Введите номер прокси которой будет использоваться, Если вы не хотите использовать прокси введите 0 ")
         if proxy_number != 0:
+            csv_accounts = csv.reader(open('proxy.csv', "r"), delimiter=",")
             for cpass in csv_accounts:
-                if numbering == proxy_number:
-                    proxy_server = cpass[0]
-                    proxy_port = cpass[1]
-                    proxy_key = cpass[2]
+                if numbering == int(proxy_number):
+                    proxy_type = cpass[0]
+                    proxy_ip = cpass[1]
+                    proxy_port = cpass[2]
+                    proxy_login = cpass[3]
+                    proxy_pass = cpass[4]
+                    g = 1
+                    print(g)
                 numbering += 1
-                g = 1
+            
             if g == 0:
-                print("Прокси нет, подключаемся без них")
+                print()
                 client = TelegramClient(phone, api_id, api_hash)
             else:
-                proxy = (proxy_server, proxy_port, proxy_key)
-                client = TelegramClient(phone, api_id, api_hash, connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,proxy=proxy)
+                print(int(proxy_type) == 2)
+                if int(proxy_type) == 1:
+                    proxy = {
+                        'proxy_type': python_socks.ProxyType.HTTP,
+                        'addr': proxy_ip,
+                        'port': int(proxy_port), 
+                        'username': proxy_login,      
+                        'password': proxy_pass,      
+                        'rdns': True            
+                    }
+                else:
+                    proxy = {
+                        'proxy_type': python_socks.ProxyType.SOCKS5,
+                        'addr': proxy_ip,
+                        'port': int(proxy_port),
+                        'username': proxy_login,
+                        'password': proxy_pass,
+                        'rdns': True
+                    }
+                client = TelegramClient(phone, api_id, api_hash ,proxy=proxy)
         else:
             client = TelegramClient(phone, api_id, api_hash)
         return client
 
 
-    def send_sms(g, file_name):
-        try:
-            cpass = configparser.RawConfigParser()
-            cpass.read('config.data')
-            csv_accounts = csv.reader(open('accounts.csv', "r"), delimiter=",")
-            for cpass in csv_accounts:
-                api_id = cpass[0]
-                api_hash = cpass[1]
-                phone = cpass[2]
-        except KeyError:
-            os.system('clear')
-            main.banner()
-            print(re+"[!] run python3 setup.py first !!\n")
-            sys.exit(1)
+    def send_sms(message, api_id, api_hash, phone):
 
         client = main.connectionTelegramAccount(phone, api_id, api_hash)
          
@@ -83,8 +97,9 @@ class main():
         
         os.system('clear')
         main.banner()
+        input_file = sys.argv[1]
         users = []
-        with open(file_name, encoding='UTF-8') as f:
+        with open(input_file, encoding='UTF-8') as f:
             rows = csv.reader(f,delimiter=",",lineterminator="\n")
             next(rows, None)
             for row in rows:
@@ -96,9 +111,6 @@ class main():
                 users.append(user)
         print(gr+"[1] send sms by user ID\n[2] send sms by username ")
         mode = int(input(gr+"Input : "+re))
-         
-        message = input(gr+"[+] Enter Your Message : "+re)
-
         for user in users:
             if mode == 2:
                 if user['username'] == "":
@@ -125,12 +137,52 @@ class main():
                 continue
         client.disconnect()
         print("Done. Message sent to all users.")
+    
+    def sms_multiaccount(message):
+        csv_accounts_file = open("accounts.csv","r+") 
+        reader_file = csv.reader(csv_accounts_file) 
+        value = len(list(reader_file)) 
+        input_file = sys.argv[1]
+        temp = configparser.RawConfigParser()
+        csv_accounts = csv.reader(open('accounts.csv', "r"), delimiter=",")
+        g = 0
+        for row in csv_accounts:
+            api_id = row[0]
+            api_hash = row[1]
+            phone = row[2]
+            invite_count = row[3]
+            break
+        
+        client = main.connectionTelegramAccount(phone, api_id, api_hash)
+         
+        client.connect()
+        if not client.is_user_authorized():
+            client.send_code_request(phone)
+            os.system('clear')
+            main.banner()
+            client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+        
+        csv_accounts = csv.reader(open('accounts.csv', "r"), delimiter=",")
+        for cpass in csv_accounts:
+            api_id = cpass[0]
+            api_hash = cpass[1]
+            phone = cpass[2]
+            invite_count = cpass[3]
+            temp = configparser.RawConfigParser()
+            try:
+                temp.read('config.data')
+                start_value = int(temp.get('START_value', 'invite'))
+            except:
+                temp.add_section('START_value')
+                temp.set('START_value', 'invite', 0)
+                setup = open('config.data', 'w')
+                temp.write(setup)
+                setup.close()
+                start_value = 0
+            main.send_sms(message, api_id, api_hash, phone)
+        
+        
 
-    def sms_multiaccount():
-        cpass = configparser.RawConfigParser()
-        cpass.read('config.data')
-        for i in range(len(cpass)):
-            main.send_sms(i, "members.csv")
 
-
-main.sms_multiaccount()
+message = input(gr+"[+] Enter Your Message : "+re)
+main.sms_multiaccount(message)
